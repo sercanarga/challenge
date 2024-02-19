4c2f8fb6659a2a7eaa1871ce1516723fcaf44c26
can <mail@sercanarga.com>
2024-02-19 20:12:52 +0300

    [GET]/ getwallets modified to support multiple balances

--- internal/routes/get_wallets.go
+++ internal/routes/get_wallets.go
@@ -3,7 +3,6 @@ package routes
 import (
 	"challenge/internal/durable"
 	"challenge/internal/models"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"strconv"
 	"time"
@@ -14,7 +13,7 @@ import (
 // @Produce		json
 // @Param		limit		query		int		false	"default:10"
 // @Param		cursor		query		int		false	"default:0"
-// @Success		200		{object}	models.APIReturn
+// @Success		200		{object}	models.WalletStruct
 // @Failure		500		{object}	models.APIReturn
 // @Router		/		[get]
 func GetWallets(ctx *gin.Context) {
@@ -23,7 +22,7 @@ func GetWallets(ctx *gin.Context) {
 	cursor = cursor * limit
 
 	var wallets []models.Wallet
-	result := durable.Connection().Offset(cursor).Limit(limit).Find(&wallets)
+	result := durable.Connection().Preload("Balance").Offset(cursor).Limit(limit).Find(&wallets)
 	if result.Error != nil {
 		ctx.JSON(500, models.APIReturn{
 			StatusCode:   500,
@@ -33,21 +32,22 @@ func GetWallets(ctx *gin.Context) {
 		return
 	}
 
-	var response []models.WalletStruct
+	var responseWallets []models.WalletStruct
 	for _, wallet := range wallets {
-		response = append(response, models.WalletStruct{
-			Id:     wallet.Id,
-			UserId: wallet.UserId,
-			Balances: []models.Balance{
-				{
-					Currency: wallet.Currency,
-					Amount:   fmt.Sprintf("%.2f", wallet.Amount),
-				},
-			},
+		var balances []models.Balance
+		for _, balance := range wallet.Balance {
+			balances = append(balances, models.Balance{
+				Currency:   balance.Currency,
+				Amount:     balance.Amount,
+				LastUpdate: balance.LastUpdate,
+			})
+		}
+		responseWallets = append(responseWallets, models.WalletStruct{
+			Id:       wallet.Id,
+			UserId:   wallet.UserId,
+			Balances: balances,
 		})
 	}
 
-	ctx.JSON(200, models.APIWalletsReturn{
-		Wallets: response,
-	})
+	ctx.JSON(200, responseWallets)
 }
